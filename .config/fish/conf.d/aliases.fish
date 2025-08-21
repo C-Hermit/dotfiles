0b6350be3007a3212bf4fcaea09bc23f8b79f9ff
hermit <cai3236513802@qq.com>
2025-08-21 16:08:03 +0800

    add aliases.fish fot fish

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -3,4 +3,13 @@
 #since 2025
 #aliases for fish
 
+command -v nvim >/dev/null && [ -z "$DVTM" ] && alias vim="nvim" && alias vimdiff="nvim -d"
 
+if status is-interactive
+
+	# stow alias
+	alias heartstow='cd ~/doc/heart && stow -t ~ . --adopt && cd - && echo "heart is stow" '
+	alias heartunstow='cd ~/doc/heart && stow -D -t ~ . && cd - && echo "heart is unstow" '
+	alias heartrestow='cd ~/doc/heart && stow -R -t ~ . && cd -  && echo "heart is unstow" '
+
+end
