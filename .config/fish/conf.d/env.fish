6df408866584554236af3308ccac18244c0fea74
hermit <cai3236513802@qq.com>
2025-08-21 16:08:55 +0800

    add env.fish fot fish

---
+++ .config/fish/conf.d/env.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+#@author hermit
+#@since 2025
+#global variable for fish
+set -gx EDITOR nvim
+set -gx MSYS winsymlinks:nativestrict
+
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,3 @@
-# 环境变量
-set -gx EDITOR nvim
-set -gx MSYS winsymlinks:nativestrict
-
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
